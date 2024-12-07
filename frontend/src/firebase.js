194a1a4c6cb67ce94cfde2589c1bfe77d843b695
AVIK MALLICK <68346978+Aviiiik@users.noreply.github.com>
2024-12-08 00:57:33 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 import 'firebase/compat/auth'
 // Your web app's Firebase configuration
 const firebaseConfig =  {
-  apiKey: "AIzaSyAHzuZDC6ouAKqRds7jDJVPNOyxlV2TqLQ",
-  authDomain: "kickoff-6c523.firebaseapp.com",
-  projectId: "kickoff-6c523",
-  storageBucket: "kickoff-6c523.firebasestorage.app",
-  messagingSenderId: "1043212590204",
-  appId: "1:1043212590204:web:d6ff187df70455ed8f7df9",
-  measurementId: "G-1M6G8J29HR"
+  // Enter your firebase api key
 };
 
 // Initialize Firebase

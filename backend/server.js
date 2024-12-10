953747b185798531083acf84eae736af49bd9649
AVIK MALLICK <68346978+Aviiiik@users.noreply.github.com>
2024-12-10 07:54:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());

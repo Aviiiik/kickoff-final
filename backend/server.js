5ebd85fb574b3724c79654aa3e356dda24b0ae43
AVIK MALLICK <68346978+Aviiiik@users.noreply.github.com>
2024-12-10 07:53:56 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'event_management'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect(err => {
